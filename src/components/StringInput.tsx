f347135be16340fb33747c559c02f3082d21994a
Jan Cernik <jancernik12@gmail.com>
2025-11-10 08:09:15 -0300

    Simple input components

---
+++ src/components/StringInput.tsx
@@ -0,0 +1,34 @@
+interface Props {
+  onChange: (value: string) => void;
+  value: string;
+  placeholder?: string;
+  name: string;
+  type: 'text' | 'email' | 'password';
+  disabled?: boolean;
+  required?: boolean;
+}
+
+function StringInput({
+  onChange,
+  placeholder,
+  name,
+  disabled,
+  required,
+  type,
+  value,
+}: Props) {
+  return (
+    <input
+      type={type}
+      name={name}
+      disabled={disabled}
+      required={required}
+      value={value}
+      placeholder={placeholder}
+      onChange={(e) => onChange(e.target.value)}
+      className="p-2 border-2 focus-within:outline-1 disabled:opacity-60 leading-none font-size outline-(--foreground)"
+    />
+  );
+}
+
+export default StringInput;
---
+++ src/components/TextArea.tsx
@@ -0,0 +1,34 @@
+interface Props {
+  onChange: (value: string) => void;
+  value: string;
+  placeholder?: string;
+  name: string;
+  disabled?: boolean;
+  required?: boolean;
+  rows?: number;
+}
+
+function TextArea({
+  onChange,
+  placeholder,
+  name,
+  disabled,
+  required,
+  rows,
+  value,
+}: Props) {
+  return (
+    <textarea
+      name={name}
+      disabled={disabled}
+      placeholder={placeholder}
+      required={required}
+      rows={rows}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      className="p-2 border-2 resize-none disabled:opacity-60 focus-within:outline-1 leading-none font-size outline-(--foreground)"
+    />
+  );
+}
+
+export default TextArea;
